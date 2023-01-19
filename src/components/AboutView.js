89fbba14c1f1a85d42629fe960feb4079507a75c
Prof-Percival <percival.mrd921@yahoo.com>
2023-01-19 17:42:28 +0200

    Adding About View Component

---
+++ src/components/AboutView.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+
+const AboutView = () => {
+  return (
+    <>
+      <Hero text="About us" />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 my-5">
+            <p className="lead">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta incidunt perspiciatis nihil laboriosam ad saepe ipsum obcaecati temporibus ipsam! Velit soluta dignissimos quasi perspiciatis voluptatum nostrum officiis expedita autem doloremque perferendis qui eius, fugit, iste animi nobis veniam harum minus dolor! Tempore quam a dolore qui, voluptas assumenda nobis omnis quod ex et nemo accusamus, temporibus veritatis consequuntur blanditiis earum. Quidem architecto perferendis quis. Mollitia a facere, accusantium, quasi, dolorem aperiam dolore adipisci inventore omnis fugit praesentium. Velit corporis suscipit magni repellendus impedit. Est, repudiandae dolore. Rerum animi, quod nulla nam impedit, expedita doloribus ratione dicta laborum incidunt magni dolore.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AboutView;
