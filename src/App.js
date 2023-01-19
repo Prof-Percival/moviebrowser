9d29a1c5d2ec0fdad877614a1ab37bdecaed15b6
Prof-Percival <percival.mrd921@yahoo.com>
2023-01-19 17:45:21 +0200

    Package and Paths changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+//TMDB API Key 7ca0618e400bd1b5bcb0085252d6621f
+//URL: https://api.themoviedb.org/3/movie/550?api_key=7ca0618e400bd1b5bcb0085252d6621f
+
+//REQUEST URL: https://api.themoviedb.org/3/search/movie?api_key=7ca0618e400bd1b5bcb0085252d6621f&language=en-US&query=star%20wars&page=1&include_adult=false
+import "./App.css";
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AboutView from "./components/AboutView";
+import SearchView from "./components/SearchView";
+import MovieView from "./components/MovieView";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    if (searchText) {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=7ca0618e400bd1b5bcb0085252d6621f&language=en-US&query=${searchText}&page=1&include_adult=false`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          /*
+          console.log(data);
+          let newData = data.results;
+          let currPage = data.page + 1;
+          let lastPage = data.total_pages;
+          console.log(newData[0]);
+          while (currPage <= lastPage){
+            const newUrl = `https://api.themoviedb.org/3/search/movie?api_key=7ca0618e400bd1b5bcb0085252d6621f&language=en-US&query=${searchText}&page=${currPage}&include_adult=false`;
+
+            fetch(newUrl)
+            .then(response => response.json())
+            .then(data => {
+
+              for (const key in data.results){
+               // newData = newData.push(data[key]);
+              }
+              
+            });
+
+            currPage++;
+            
+          }
+          */
+
+          setSearchResults(data.results);
+          //console.log(newData);
+        });
+    }
+  }, [searchText]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar searchText={searchText} setSearchText={setSearchText} />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about" component={AboutView} />
+        <Route path="/search">
+          <SearchView keyword={searchText} searchResults={searchResults} />
+        </Route>
+        <Route path="/movies/:id" component={MovieView} />
+      </Switch>
     </div>
   );
 }
