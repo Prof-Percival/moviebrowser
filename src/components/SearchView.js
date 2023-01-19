3defc2282a5bd136d3f08fb60576dc61fc3ec7b2
Prof-Percival <percival.mrd921@yahoo.com>
2023-01-19 17:44:32 +0200

    Adding Search View Component

---
+++ src/components/SearchView.js
@@ -0,0 +1,25 @@
+import Hero from "./Hero";
+import MovieCard from './MovieCard'
+
+const SearchView = ({ keyword, searchResults }) => {
+  const title = `You are searching for ${keyword}`;
+
+  const resultsHtml = searchResults.map((obj, i) => {
+    return <MovieCard movie={obj} key={i} />;
+  });
+
+  return (
+    <>
+      <Hero text={title} />
+      {resultsHtml && (
+        <div className="container">
+            <div className="row">
+                {resultsHtml}
+            </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SearchView;
